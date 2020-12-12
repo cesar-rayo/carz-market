32a2838bfea060ec2c30682558927cb2f6a00ef1
cesar-rayo <cesar-rayo@hotmail.es>
2020-12-12 14:14:32 -0500

    first commit

---
+++ src/main/java/com/carz/market/CarzMarketApplication.java
@@ -0,0 +1,13 @@
+package com.carz.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarzMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarzMarketApplication.class, args);
+	}
+
+}
